03282a344aca1a5ec8efb994a4a4cffd2838cfab
dishaagrawalcodes <dishaagrawal9568@gmail.com>
2024-11-13 00:39:43 +0530

    added keypress function now you can play siounds by pressing keys also

--- index.js
+++ index.js
@@ -37,5 +37,39 @@ for (var i = 0; i <numberofdrumbuttons; i++) {
       console.log(buttoninnerhtml);
   }
 })
-}
-
+}document.addEventListener("keydown",function(event){
+  makeSound(event.key);
+});
+function makeSound(key){
+  switch (key) {
+    case "w":
+      const audioW = new Audio('sounds/tom-1.mp3');
+      audioW.play();
+      break;
+    case "a":
+      const audioA = new Audio('sounds/tom-2.mp3');
+      audioA.play();
+      break;
+    case "s":
+      const audioS = new Audio('sounds/tom-3.mp3');
+      audioS.play();
+      break;
+    case "d":
+      const audioD = new Audio('sounds/tom-4.mp3');
+      audioD.play();
+      break;
+    case "j":
+      const audioJ = new Audio('sounds/snare.mp3');
+      audioJ.play();
+      break;
+    case "k":
+      const audioK = new Audio('sounds/crash.mp3');
+      audioK.play();
+      break;
+    case "l":
+      const audioL = new Audio('sounds/kick-bass.mp3');
+      audioL.play();
+      break;
+    default:
+      console.log(buttoninnerhtml); 
+}}
