03f506deba442e7f8bb720aef4cb94b6b0cb7afe
dishaagrawalcodes <dishaagrawal9568@gmail.com>
2024-11-13 22:08:39 +0530

    adding shadows and settimeout button

--- index.js
+++ index.js
@@ -3,42 +3,12 @@ for (var i = 0; i <numberofdrumbuttons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click",function(){
   console.log(this);
   var buttoninnerhtml =this.innerHTML;
-  this.style.color="white";
-  switch (buttoninnerhtml) {
-    case "w":
-      const audioW = new Audio('sounds/tom-1.mp3');
-      audioW.play();
-      break;
-    case "a":
-      const audioA = new Audio('sounds/tom-2.mp3');
-      audioA.play();
-      break;
-    case "s":
-      const audioS = new Audio('sounds/tom-3.mp3');
-      audioS.play();
-      break;
-    case "d":
-      const audioD = new Audio('sounds/tom-4.mp3');
-      audioD.play();
-      break;
-    case "j":
-      const audioJ = new Audio('sounds/snare.mp3');
-      audioJ.play();
-      break;
-    case "k":
-      const audioK = new Audio('sounds/crash.mp3');
-      audioK.play();
-      break;
-    case "l":
-      const audioL = new Audio('sounds/kick-bass.mp3');
-      audioL.play();
-      break;
-    default:
-      console.log(buttoninnerhtml);
-  }
+  makeSound(buttoninnerhtml);
+  buttonanimation(buttoninnerhtml)
 })
 }document.addEventListener("keydown",function(event){
   makeSound(event.key);
+  buttonanimation(event.key);
 });
 function makeSound(key){
   switch (key) {
@@ -73,3 +43,10 @@ function makeSound(key){
     default:
       console.log(buttoninnerhtml); 
 }}
+function buttonanimation(key){
+var activebutton =document.querySelector("."+key);
+activebutton.classList.add("pressed");
+setTimeout(function(){
+ activebutton.classList.remove("pressed");
+} ,200);
+}
